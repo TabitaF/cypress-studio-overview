4eb915ec542da3e65e740a0d32bfab8978b7c494
TabitaF <tfuentes@arkusnexus.com>
2024-03-05 13:06:55 -0800

    segundo commit

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'ifkdqm',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  
+    "e2e": {
+      "experimentalStudio": true,
+      "chromeWebSecurity": false,
+      "video": true,
+      "projectId": "ifkdqm",
+      "recordKey": "41580c33-6ab1-4a8d-af05-77ed9fdeeb94"
+     }
+
+    
+}
+  
+);
